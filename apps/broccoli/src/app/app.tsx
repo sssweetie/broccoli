31b3e71b4e3c7e5cb08e28f8a8da5b12acb5c756
Alexandr Tokarev <63711180+sssweetie@users.noreply.github.com>
2024-01-31 19:19:48 +0300

    build: set up react & express app

--- apps/broccoli/src/app/app.tsx
+++ apps/broccoli/src/app/app.tsx
@@ -1,13 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
-    <div>
-      <NxWelcome title="broccoli" />
-    </div>
+    <div></div>
   );
 }
 
