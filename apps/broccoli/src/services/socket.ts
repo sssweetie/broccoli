4f0bbc0c69eeddad44a89512d945079517aeef23
Alexandr Tokarev <63711180+sssweetie@users.noreply.github.com>
2024-03-03 14:10:03 +0300

    WIP: sockets in progress

---
+++ apps/broccoli/src/services/socket.ts
@@ -0,0 +1,2 @@
+import * as io from 'socket.io-client';
+export const socket = io.connect('http://localhost:3001');
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,11 +2,25 @@ import './db';
 import { router } from './routes';
 import cors from 'cors';
 import express from 'express';
+import { Server } from 'socket.io';
+import http = require('http');
 
 const app = express();
 const port = 3001;
+const client = 'http://localhost:4200';
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
-app.listen(port, () => console.log(`Server is starting in port ${port}`));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: client,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  },
+});
+
+
+server.listen(port, () => console.log(`Server is starting in port ${port}`));
