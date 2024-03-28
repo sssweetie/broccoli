cbd63fc26439f0184fb3ab6558888bf7fc99cc50
Alexandr Tokarev <63711180+sssweetie@users.noreply.github.com>
2024-03-29 01:10:08 +0300

    feat: calendar header

--- apps/broccoli/src/features/Calendar/Calendar.tsx
+++ apps/broccoli/src/features/Calendar/Calendar.tsx
@@ -9,6 +9,20 @@ export const Calendar = () => {
   const subtasks = useCalendar(subtasksAPI(httpClient), dates);
 
   const calendar = renderCalendar(dates.dateFrom, subtasks);
-
-  return <div style={{ height: '100%' }}>{calendar}</div>;
+  
+  return (
+    <div style={{ height: '100%' }}>
+      <section className="calendar-navigation">
+        <h1 className="calendar-navigation__title">March 2024</h1>
+        <div className="calendar-navigation__menu">
+          <button>&#8592;</button>
+          <button>Today</button>
+          <button>&#8594;</button>
+          <button>Month</button>
+          <button>Week</button>
+        </div>
+      </section>
+      {calendar}
+    </div>
+  );
 };
